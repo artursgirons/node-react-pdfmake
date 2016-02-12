db666e4c9ef441599ac3cf4d407aac9998d87992
Arturs Girons <arturs.girons@gmail.com>
2016-02-12 15:04:13 +0200

    update

--- node/index.js
+++ node/index.js
@@ -3,7 +3,9 @@ module.exports = function() {
   var path = require('path');
   var app = express();
 
-  app.use(express.static(path.resolve(__dirname, '..', 'assets')))
+  var assetsPath = path.resolve(__dirname, path.relative(__dirname, './'), 'node_modules/node-react-pdfmake/assets')
+
+  app.use(express.static(assetsPath))
 
   return app
 }
